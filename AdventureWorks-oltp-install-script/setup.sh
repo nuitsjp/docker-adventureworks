6d3f4532a2c902381d3f968b3321f8ba688458c8
Atsushi Nakamura <nuits.jp@live.jp>
2019-10-07 07:37:53 +0900

    update setup.sh

--- AdventureWorks-oltp-install-script/setup.sh
+++ AdventureWorks-oltp-install-script/setup.sh
@@ -2,5 +2,9 @@
 export ACCEPT_EULA=Y
 export SA_PASSWORD=P@ssw0rd!
 /opt/mssql/bin/sqlservr &
+
+#wait for the SQL Server to come up
+sleep 90s
+
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd! -d master -i /work/instawdb.sql
 # & /usr/src/app/import-data.sh & npm start 
