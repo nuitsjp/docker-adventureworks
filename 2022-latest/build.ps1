dea1a2e5db25239584c539311d859c41b5542671
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2022-09-05 11:23:09 +0900

    SQL Server 2022へ移行

--- 2022-latest/build.ps1
+++ 2022-latest/build.ps1
@@ -1,1 +1,4 @@
 docker build -t nuitsjp/adventureworks:2022-latest .
+# docker push nuitsjp/adventureworks:2022-latest
+docker build -t nuitsjp/adventureworks:latest .
+# docker push nuitsjp/adventureworks:latest
